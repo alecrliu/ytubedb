3de5bbd62692c00312a5d543391f60c85556600c
Junyu Yao <yaojunyu@utexas.edu>
2024-02-07 11:19:19 -0600

    Changed port number

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ def index():
 # debug=True to avoid restart the local development server manually after each change to your code. 
 # host='0.0.0.0' to make the server publicly available. 
 if __name__ == "__main__":
-	app.run(debug=True, host='0.0.0.0',port='5003')
+	app.run(debug=True, host='0.0.0.0')
