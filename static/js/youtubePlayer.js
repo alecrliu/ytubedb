998e8fb413602b854b97a8a935226ee9f71e2846
Alec Liu <alec.liu@utexas.edu>
2024-02-14 10:13:21 -0600

    moved youtubePlayer script out of video.html

---
+++ static/js/youtubePlayer.js
@@ -0,0 +1,42 @@
+
+// {/* This code loads the IFrame Player API code asynchronously. */ }
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// {/* This function creates an <iframe> (and YouTube player) after the API code downloads. */ }
+var players = [];
+function onYouTubeIframeAPIReady() {
+    for (var i = 0; i < players.length; i++) {
+        players[i] = new YT.Player('player' + (i + 1), {
+            height: '400',
+            width: '100%',
+            videoId: players[i],
+            playerVars: {
+                'playsinline': 1
+            },
+            events: {
+                'onReady': onPlayerReady,
+                'onStateChange': onPlayerStateChange
+            }
+        });
+    }
+}
+
+// The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+// The API calls this function when the player's state changes.
+var done = false;
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+function stopVideo() {
+    player.stopVideo();
+}
