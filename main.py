e32527ffb3f1f7844efe726a0a2c8e715f91fdf0
Junyu Yao <yaojunyu@utexas.edu>
2024-02-20 18:43:52 -0600

    # Add detail playlist page, link to channel and video

--- main.py
+++ main.py
@@ -17,23 +17,23 @@ with open('data/video.json', 'r', encoding='utf-8') as file:
 # index
 # ------------
 
-
+# splash page
 @app.route('/')
 def index():
     return render_template('splash.html')
 
-
+# about page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-
+# channels page display multiple channels
 @app.route('/channels')
 def showChannels():
     channels_info = channel_data['items']
     return render_template('channels.html', channels=channels_info)
 
-
+# channel page display detail channel info
 @app.route('/channel/<string:channelId>')
 def showChannel(channelId):
     # Directly use channel_data['items'] if channels is not defined elsewhere
@@ -43,12 +43,12 @@ def showChannel(channelId):
         return "Channel not found", 404
     return render_template('channel.html', channel=channel_info)
 
-
+# videos page display multiple videos
 @app.route('/videos')
 def showVideos():
     return render_template('videos.html', videos=videos["items"])
 
-
+# video page display single video
 @app.route('/video/<string:videoId>')
 def playVideo(videoId):
     video_info = None
@@ -60,9 +60,21 @@ def playVideo(videoId):
     return render_template('video.html', video=video_info, videoId=videoId)
 
 
+# playlists page display mutiple videos
 @app.route('/playlists')
 def showPlaylist():
-    return render_template('playlist.html', playlists=playlist_data['items'])
+    return render_template('playlists.html', playlists=playlist_data['items'])
+
+# playlists page display single playlist
+@app.route('/playlist/<string:playlistId>')
+def playList(playlistId):
+    playlist_info = None
+    playlist_list = playlist_data["items"]
+    for playlist in playlist_list:
+        if playlist['id'] == playlistId:
+            playlist_info = playlist
+            break
+    return render_template('playlist.html', playlist=playlist_info, playlistId=playlistId)
 
 
 # debug=True to avoid restart the local development server manually after each change to your code.
