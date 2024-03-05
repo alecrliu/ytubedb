8076e727658913d374ca2c3e4fbfea664adeb91a
Adrian Barajas <adrian.barajas@utexas.edu>
2024-03-05 17:55:22 -0600

    Implemented channels table in models.py

--- models.py
+++ models.py
@@ -32,3 +32,17 @@ db = SQLAlchemy(app)
 # 	id = db.Column(db.Integer, primary_key = True)
 
 # db.create_all()
+
+class Channel(db.Model):
+    __tablename__ = 'channels'
+    
+    channelID = db.Column(db.String, primary_key=True)
+    videoID = db.Column(db.String, db.ForeignKey('videos.videoID')) 
+    playlistID = db.Column(db.String, db.ForeignKey('playlists.playlistID'))  
+    channelName = db.Column(db.String, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    subscriberCount = db.Column(db.BigInteger, nullable=True)  
+    videoCount = db.Column(db.Integer, nullable=True)  
+    thumbnail = db.Column(db.Text, nullable=True)
+    
+db.create_all()
