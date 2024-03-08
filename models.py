a54dcd553b128d47012aa30903c161053842b239
nirmpatel <nirmpatel@utexas.edu>
2024-03-08 14:07:44 -0600

    Tables defined

--- models.py
+++ models.py
@@ -1,10 +1,8 @@
 """Creates classes (tables) for each model in the database"""
 
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
-
 
 # initializing Flask app
 app = Flask(__name__)
@@ -18,40 +16,66 @@ PUBLIC_IP_ADDRESS = "localhost:5432"
 DBNAME = "ytubedb"
 
 # Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    os.environ.get(
-        "DB_STRING", f'postgresql://{USER}:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+	"DB_STRING", f'postgresql://{USER}:{PASSWORD}@{PUBLIC_IP_ADDRESS}/{DBNAME}'
+	)
 # To suppress a warning message
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
-
-# uncomment playlistID lines after creating playlists table
-class Video(db.Model):
-    __tablename__ = 'videos'
-
-    videoID = db.Column(db.String, primary_key=True)
-    channelID = db.Column(db.String, db.ForeignKey('channels.channelID'))
-    # playlistID = db.Column(db.String, db.ForeignKey('playlists.playlistID'))
-    title = db.Column(db.String, nullable=False)
-    description = db.Column(db.Text, nullable=False)
-    views = db.Column(db.BigInteger, nullable=False)
-    likes = db.Column(db.Integer, nullable=False)
-    commentCount = db.Column(db.Integer, nullable=False)
-    thumbnail = db.Column(db.Text, nullable=False)
-
-
+# Channel table
+# One-to-Many with Video table
+# One-to-Many with Playlist table
 class Channel(db.Model):
-    __tablename__ = 'channels'
+	__tablename__ = 'channels'
 
-    channelID = db.Column(db.String, primary_key=True)
-    videoID = db.Column(db.String, db.ForeignKey('videos.videoID'))
-    # playlistID = db.Column(db.String, db.ForeignKey('playlists.playlistID'))
-    channelName = db.Column(db.String, nullable=False)
-    description = db.Column(db.Text, nullable=True)
-    subscriberCount = db.Column(db.BigInteger, nullable=True)
-    videoCount = db.Column(db.Integer, nullable=True)
-    thumbnail = db.Column(db.Text, nullable=True)
+	channelID = db.Column(db.String, primary_key=True)
+	channelName = db.Column(db.String, nullable=False)
+	description = db.Column(db.Text, nullable=True)
+	subscriberCount = db.Column(db.Integer, nullable=False)
+	viewCount = db.Column(db.BigInteger, nullable=False)
+	videoCount = db.Column(db.Integer, nullable=False)
+	thumbnail = db.Column(db.Text, nullable=True)
 
+	videos = db.relationship('Video', backref = 'channels')
+	playlists = db.relationship('Playlist', backref = 'channels')
+
+# Video table
+# One-to-Many with VideoPlaylist table => Many-to-Many with Playlist table
+class Video(db.Model):
+	__tablename__ = 'videos'
+
+	videoID = db.Column(db.String, primary_key=True)
+	channelID = db.Column(db.String, db.ForeignKey('channels.channelID'))
+	title = db.Column(db.String, nullable=False)
+	description = db.Column(db.Text, nullable=True)
+	viewCount = db.Column(db.BigInteger, nullable=False)
+	likeCount = db.Column(db.Integer, nullable=False)
+	commentCount = db.Column(db.Integer, nullable=False)
+	thumbnail = db.Column(db.Text, nullable=True)
+
+	playlists = db.relationship('VideoPlaylist', backref = 'videos')
+
+# Playlist table
+# One-to-Many with VideoPlaylist table => Many-to-Many with Video table
+class Playlist(db.Model):
+	__tablename__ = 'playlists'
+
+	playlistID = db.Column(db.String, primary_key=True)
+	channelID = db.Column(db.String, db.ForeignKey('channels.channelID'))
+	title = db.Column(db.String, nullable=False)
+	description = db.Column(db.Text, nullable=True)
+	publishedAt = db.Column(db.DateTime(timezone=False), nullable=False)
+	videoCount = db.Column(db.Integer, nullable=False)
+	thumbnail = db.Column(db.Text, nullable=True)
+
+	videos = db.relationship('VideoPlaylist', backref = 'playlists')
+
+# Video-Playlist association table
+class VideoPlaylist(db.Model):
+	__tablename__ = 'videoplaylist'
+
+	videoID = db.Column(db.String, db.ForeignKey('videos.videoID'), primary_key=True)
+	playlistID = db.Column(db.String, db.ForeignKey('playlists.playlistID'), primary_key=True)
 
 db.create_all()
