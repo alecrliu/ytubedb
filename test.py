91c31485e114648c7ccf36813bbe92864dba5fc2
nirmpatel <nirmpatel@utexas.edu>
2024-03-22 16:56:39 -0500

    Playlist tests done

--- test.py
+++ test.py
@@ -6,6 +6,16 @@ import sys
 import unittest
 from models import db, Channel, Playlist, Video
 
+CHANNEL_DATA = {
+    "channel_id": "ChABC",
+    "channelName": "ABC",
+    "description": "ABC",
+    "subscriberCount": 0,
+    "viewCount": 0,
+    "videoCount": 0,
+    "thumbnail": "ABC",
+}
+
 class DBTestCases(unittest.TestCase):
     def test1_playlist(self):
         playlist_data = {
@@ -15,10 +25,12 @@ class DBTestCases(unittest.TestCase):
             "publishedAt": "2023-12-11T15:57:10Z",
             "videoCount": 0,
             "thumbnail": "THUMBNAIL",
-            "channel_id": "UC-lHJZR3Gqxm24_Vd_AJ5Yw"
+            "channel_id": CHANNEL_DATA["channel_id"]
         }
-        s = Playlist(**playlist_data)
-        db.session.add(s)
+        c = Channel(**CHANNEL_DATA)
+        db.session.add(c)
+        test_playlist = Playlist(**playlist_data)
+        db.session.add(test_playlist)
         db.session.commit()
         r = db.session.query(Playlist).filter_by(playlist_id=playlist_data["playlist_id"]).one()
         self.assertEqual(r.playlist_id, playlist_data["playlist_id"])
@@ -29,6 +41,7 @@ class DBTestCases(unittest.TestCase):
         self.assertEqual(r.thumbnail, playlist_data["thumbnail"])
         self.assertEqual(r.channel_id, playlist_data["channel_id"])
         db.session.query(Playlist).filter_by(playlist_id=playlist_data["playlist_id"]).delete()
+        db.session.query(Channel).filter_by(channel_id=CHANNEL_DATA["channel_id"]).delete()
         db.session.commit()
     
     def test2_playlist(self):
@@ -39,7 +52,7 @@ class DBTestCases(unittest.TestCase):
             "publishedAt": "2023-12-11T15:57:10Z",
             "videoCount": 0,
             "thumbnail": "THUMBNAIL",
-            "channel_id": "UC-lHJZR3Gqxm24_Vd_AJ5Yw"
+            "channel_id": CHANNEL_DATA["channel_id"]
         }
         playlist_data2 = {
             "playlist_id": "A2",
@@ -48,16 +61,19 @@ class DBTestCases(unittest.TestCase):
             "publishedAt": "2023-12-11T15:57:10Z",
             "videoCount": 0,
             "thumbnail": "THUMBNAIL",
-            "channel_id": "UC-lHJZR3Gqxm24_Vd_AJ5Yw"
+            "channel_id": CHANNEL_DATA["channel_id"]
         }
-        s1 = Playlist(**playlist_data1)
-        s2 = Playlist(**playlist_data2)
-        db.session.add(s1)
-        db.session.add(s2)
+        c = Channel(**CHANNEL_DATA)
+        db.session.add(c)
+        test_playlist1 = Playlist(**playlist_data1)
+        test_playlist2 = Playlist(**playlist_data2)
+        db.session.add(test_playlist1)
+        db.session.add(test_playlist2)
         db.session.commit()
         r = db.session.query(Playlist).filter_by(thumbnail="THUMBNAIL").all()
         self.assertEqual(len(r), 2)
         db.session.query(Playlist).filter_by(thumbnail="THUMBNAIL").delete()
+        db.session.query(Channel).filter_by(channel_id=CHANNEL_DATA["channel_id"]).delete()
         db.session.commit()
     
     def test3_playlist(self):
@@ -68,15 +84,18 @@ class DBTestCases(unittest.TestCase):
             "publishedAt": "2023-12-11T15:57:10Z",
             "videoCount": 0,
             "thumbnail": "THUMBNAIL",
-            "channel_id": "UC-lHJZR3Gqxm24_Vd_AJ5Yw"
+            "channel_id": CHANNEL_DATA["channel_id"]
         }
-        s = Playlist(**playlist_data)
+        c = Channel(**CHANNEL_DATA)
+        db.session.add(c)
+        test_playlist = Playlist(**playlist_data)
         before_count = db.session.query(Playlist).count()
-        db.session.add(s)
+        db.session.add(test_playlist)
         db.session.commit()
         after_count = db.session.query(Playlist).count()
         self.assertEqual(after_count, before_count + 1)
         db.session.query(Playlist).filter_by(playlist_id=playlist_data["playlist_id"]).delete()
+        db.session.query(Channel).filter_by(channel_id=CHANNEL_DATA["channel_id"]).delete()
         db.session.commit()
 
 
