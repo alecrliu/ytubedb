e2ec75a89623f3b902bf813ce537d68f5ea760c3
nirmpatel <nirmpatel@utexas.edu>
2024-04-03 17:25:30 -0500

    API code refactored

--- youtubeData.py
+++ youtubeData.py
@@ -28,7 +28,8 @@ def get_one_channel(channel_id, youtube):
         video_count = channel_data["statistics"].get("videoCount", 0)
         thumbnail_url = "No thumbnail available"
         if channel_data["snippet"].get("thumbnails"):
-            thumbnail_url = channel_data["snippet"].get("thumbnails")["high"]["url"]
+            thumbnail_url = channel_data["snippet"].get("thumbnails")[
+                "high"]["url"]
             if channel_data["snippet"]["thumbnails"].get("maxres"):
                 thumbnail_url = channel_data["snippet"]["thumbnails"]["maxres"]["url"]
         channelData["channelID"] = channel_id
@@ -63,7 +64,8 @@ def get_one_video(video_id, channel_id, youtube, checkChannelID=True):
         comment_count = video_data["statistics"].get("commentCount", 0)
         thumbnail_url = "No thumbnail available"
         if video_data["snippet"].get("thumbnails"):
-            thumbnail_url = video_data["snippet"].get("thumbnails")["medium"]["url"]
+            thumbnail_url = video_data["snippet"].get("thumbnails")[
+                "medium"]["url"]
             if video_data["snippet"]["thumbnails"].get("maxres"):
                 thumbnail_url = video_data["snippet"]["thumbnails"]["maxres"]["url"]
         videoData["videoID"] = video_id
@@ -131,7 +133,8 @@ def get_one_playlist(playlist_response, channel_id):
         published_at = playlist_response["snippet"]["publishedAt"]
         thumbnail_url = "No thumbnail available"
         if playlist_response["snippet"].get("thumbnails"):
-            thumbnail_url = playlist_response["snippet"].get("thumbnails")["medium"]["url"]
+            thumbnail_url = playlist_response["snippet"].get("thumbnails")[
+                "medium"]["url"]
             if playlist_response["snippet"]["thumbnails"].get("maxres"):
                 thumbnail_url = playlist_response["snippet"]["thumbnails"]["maxres"]["url"]
         playlistData["playlistID"] = playlist_id
